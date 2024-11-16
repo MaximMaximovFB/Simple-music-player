e096771905884b7531a7a1025af60f16209433c7
Maxim Mikula <mikmax.2003@gmail.com>
2024-11-16 15:16:32 +0300

    Getting started with the Main screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,32 @@
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router';
+// import { FlatList } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Welcome = () => {
 
   return (
-    <View className = "flex-1 justify-center items-center bg-white">
-      <Text className = "text-3xl font-scExtraBold">Main screen title!!!</Text>
-      <Link href="/profile" className="color-red-800"> Go to Profile</Link>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data= {[{ id: 1},{ id: 2},{ id: 3}, ]}
+        keyExtractor={(item) => item.$id}
+        renderItem={( { item }) => (
+          <Text className="text-3xl font-scExtraBold"> {item.id}</Text>
+        )}
+        ListHeaderComponent={( ) => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between  items-start flex-row mb-6">
+              <View>
+                <Text className=''>
+                  Здесь будут кнопки!
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
